671f3227e5c09d1a409fe615d7fc6818e3d35232
Yan Pujante <yan@pongasoft.com>
2023-05-15 09:30:40 -0700

    use re-mock 1.4.3
    - re-edit can now be built without installing the RE SDK!

--- cmake/fetch_re-mock.cmake
+++ cmake/fetch_re-mock.cmake
@@ -6,9 +6,9 @@ include(cmake/REEditFetchContent.cmake)
 # Git repo/tag for re-mock
 #------------------------------------------------------------------------
 set(re-mock_GIT_REPO "https://github.com/pongasoft/re-mock" CACHE STRING "re-mock git repository url")
-set(re-mock_GIT_TAG "v1.4.2" CACHE STRING "re-mock git tag")
+set(re-mock_GIT_TAG "v1.4.3" CACHE STRING "re-mock git tag")
 set(re-mock_DOWNLOAD_URL "${re-mock_GIT_REPO}/archive/refs/tags/${re-mock_GIT_TAG}.zip" CACHE STRING "re-mock download url" FORCE)
-set(re-mock_DOWNLOAD_URL_HASH "SHA256=f19e41cde101334e5c99adebba17d23eed08aa3039120d6c0794402f3ee491b8" CACHE STRING "re-mock download url hash" FORCE)
+set(re-mock_DOWNLOAD_URL_HASH "SHA256=251340fdc1ddb4a44b55ab4b55e9dad3f3b1d4de70a46af183577523ca87e27a" CACHE STRING "re-mock download url hash" FORCE)
 
 re_edit_fetch_content(NAME re-mock)
 
