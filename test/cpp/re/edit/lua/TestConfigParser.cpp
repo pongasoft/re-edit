9ec5fc70c977bf6da155138fdafd2fa93d26cba9
Yan Pujante <yan@pongasoft.com>
2023-08-12 08:46:26 -0700

    fixed failing test

--- test/cpp/re/edit/lua/TestConfigParser.cpp
+++ test/cpp/re/edit/lua/TestConfigParser.cpp
@@ -28,6 +28,8 @@ TEST(ConfigParser, All) {
 global_config = {}
 global_config["font_size"] = 20
 global_config["style"] = "Dark"
+global_config["target_frame_rate"] = 60
+global_config["vsync_enabled"] = false
 global_config["device_history"] = {}
 global_config["device_history"][1] = {
   name = "CVA-7 CV Analyzer",
